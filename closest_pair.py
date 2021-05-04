9a3ca62e67b84e7eedc6c1446e30f68141e3b1cf
Nicole Pevalova <nicole.pevalova@gmail.com>
2021-05-04 19:03:10 +0200

    jezisovenohykonecnekoniec

--- closest_pair.py
+++ closest_pair.py
@@ -26,6 +26,10 @@ def read_file(file_name):
     return data_points
 
 
+
+
+
+
 def draw_data(data_points, closest_pair=[]):
     """
     Function creates new figure and draw data points into scatter plot.
@@ -58,6 +62,45 @@ def closest_pair_BF(array):
                     point_1, point_2 = array[i], array[j]
     return point_1, point_2, min_dist
 
+def closest_split_pair(x_wise, y_wise, min_dist, points):
+    num_points = len(x_wise)
+    midpointX = x_wise[num_points // 2][0]
+
+    subarrayY = list()
+    for item in y_wise:
+        if (midpointX - min_dist) <= item[0] <= (midpointX + min_dist):
+            subarrayY.append(item)
+
+def closest_pair(x_wise, y_wise):
+    num_points = len(x_wise)
+    sterdny_idx = num_points // 2
+
+    if num_points <= 3:
+        point1, point2, min_dist = closest_pair_BF(x_wise)
+        return point1, point2, min_dist
+
+    left_x = x_wise[:sterdny_idx]
+    right_x = x_wise[sterdny_idx:]
+
+    mid_point = x_wise[sterdny_idx][0]
+    left_y = list()
+    right_y = list()
+
+    for point in y_wise:
+        if point[0] <= sterdny_idx:
+            left_y.append(point)
+        else:
+            right_y.append(point)
+
+    (p1, q1, m1) = closest_pair(left_x, left_y)
+    (p2, q2, m2) = closest_pair(right_x, right_y)
+
+    if m1 <= m2:
+        min_dist = m1
+        points = (p1, q1)
+    else:
+        min_dist = m2
+        points = (p2, q2)
 
 def main(file_name):
     # read data points
