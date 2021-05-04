c7c29f9d27a6e0ffadd36fa9f42b5f7e10cc2b1f
Nicole Pevalova <nicole.pevalova@gmail.com>
2021-05-04 17:22:28 +0200

    prva cast

--- fibo.py
+++ fibo.py
@@ -1,7 +1,17 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return  0
+    elif n == 1:
+        return 1
+    else:
+        return (recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2))
+
+
+
 
 def main():
-    pass
-
+    prvok = int(input("Zadaj prvok: "))
+    print(recursive_nth_fibo(prvok))
 
 if __name__ == '__main__':
     main()
